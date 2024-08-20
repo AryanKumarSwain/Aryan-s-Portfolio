628fa83c7b06eefb5ea99ab453c89570ef6bb31d
Aryan <aryannn.ks@gmail.com>
2024-08-20 18:46:54 +0530

    html

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <div id="projects"  className="border-b border-neutral-800  pb-24 ">
       <h2 className="my-20 text-center text-4xl ">Projects</h2>
 
-      <h4 className=' text-center text-8xl m-40' > Coming Soon... </h4>
+      <h4 className='flex flex-wrap items-center  justify-center gap-6  text-center text-8xl m-40' > Coming Soon... </h4>
     </div>
   )
 }
