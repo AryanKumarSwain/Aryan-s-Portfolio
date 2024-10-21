7d4491ed04511f4df2ef8bd6600baaa0cf4c11d7
Aryan <aryannn.ks@gmail.com>
2024-10-21 22:32:35 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import About from './components/About';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
@@ -6,13 +6,11 @@ import Technologies from './components/Technologies';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Projects from './components/Projects';
-// import TopDown from './components/TopDown';
 import './App.css'; // Ensure you import the CSS where the glow effect is defined
 
 const App = () => {
-
-  useEffect(() => {
-    const heroSection = document.getElementById('hero');
+  // Add scroll effect
+  React.useEffect(() => {
     const logoDark = document.querySelector('.logo-dark');
     const logoLight = document.querySelector('.logo-light');
     const socialIcons = document.querySelectorAll('.social-icon');
@@ -21,17 +19,11 @@ const App = () => {
       if (window.scrollY > 50) {
         logoDark?.classList.add('glow');
         logoLight?.classList.add('glow');
-
-        socialIcons.forEach(icon => {
-          icon.classList.add('glow');
-        });
+        socialIcons.forEach((icon) => icon.classList.add('glow'));
       } else {
         logoDark?.classList.remove('glow');
         logoLight?.classList.remove('glow');
-
-        socialIcons.forEach(icon => {
-          icon.classList.remove('glow');
-        });
+        socialIcons.forEach((icon) => icon.classList.remove('glow'));
       }
     };
 
@@ -43,7 +35,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+    <div className="overflow-x-hidden text-neutral-300 custom-cursor custom-cursor-pointer antialiased selection:bg-cyan-300 selection:text-cyan-900">
       <div className="fixed inset-0 -z-10 h-full w-full bg-light-mode dark:bg-dark-mode"></div>
 
       <div className="container w-full p-4 lg:m-auto">
@@ -52,7 +44,6 @@ const App = () => {
         <About />
         <Technologies />
         <Projects />
-        {/* <TopDown /> */}
         <Contact />
         <Footer />
       </div>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,78 +1,13 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+
 
 export const HERO_CONTENT = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
+export const PRO_1 = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
+export const PRO_2 = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
+export const PRO_3 = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
+export const PRO_4 = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
 
-export const ABOUT_TEXT = `I am a front-end developer with a deep passion for creating engaging and responsive web interfaces. My expertise lies in React.js and Tailwind CSS, and I have a strong foundation in HTML, CSS, and JavaScript. Beyond development, I am skilled in C, C++, and Python, which enhances my problem-solving capabilities. In addition to my technical skills, I am a graphic designer with a keen eye for visual aesthetics. My goal is to blend these diverse skills to craft exceptional user experiences and innovative web applications. Outside of my professional life, I enjoy playing musical instruments, including the guitar and flute. I also find joy in sketching and portrait art, which allows me to express my creativity in different ways.
+export const ABOUT_TEXT = `I am a front-end developer with a deep passion for creating engaging and responsive web interfaces. My expertise lies in React.js and Tailwind CSS, and I have a strong foundation in HTML, CSS, and JavaScript. Beyond development, I am skilled in C, C++, and Python, which enhances my problem-solving capabilities. In addition to my technical skills, I am a graphic designer with a keen eye for visual aesthetics. My goal is to blend these diverse skills to craft exceptional user experiences and innovative web applications. Outside of my professional life, I enjoy playing musical instruments, including the guitar and flute. I also find joy in sketching and portrait art, which allows me to express my creativity in different ways.`;
 
-`;
 
-export const EXPERIENCES = [
-  {
-    year: "2023 - Present",
-    role: "Senior Full Stack Developer",
-    company: "Google Inc.",
-    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
-    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
-  },
-  {
-    year: "2022 - 2023",
-    role: "Frontend Developer",
-    company: "Adobe",
-    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
-    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
-  },
-  {
-    year: "2021 - 2022",
-    role: "Full Stack Developer",
-    company: "Facebook",
-    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
-    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
-  },
-  {
-    year: "2020 - 2021",
-    role: "Software Engineer",
-    company: "Paypal",
-    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
-    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
-  },
-];
 
-export const PROJECTS = [
-  {
-    title: "E-Commerce Website",
-    image: project1,
-    description:
-      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
-  },
-  {
-    title: "Task Management App",
-    image: project2,
-    description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
-  },
-  {
-    title: "Portfolio Website",
-    image: project3,
-    description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
-  },
-  {
-    title: "Blogging Platform",
-    image: project4,
-    description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  },
-];
 
-export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
-};
