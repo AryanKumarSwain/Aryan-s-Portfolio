9b6ae73c2ef5520253dc8090d130310c3dadaa87
Aryan <aryannn.ks@gmail.com>
2024-10-22 00:06:28 +0530

    done

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,16 +1,17 @@
 
 
 export const HERO_CONTENT = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
-<<<<<<< HEAD
+
 export const PRO_1 = `This portfolio website showcases my skills as a web developer and graphic designer. It features responsive design, an interactive project showcase, and a contact form for inquiries. Built with HTML, CSS, JavaScript, React.js, and Tailwind CSS, it highlights my proficiency in creating engaging web interfaces.`;
-=======
-export const PRO_1 = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
->>>>>>> da4dbd561c0fb4828ebe940420403a09ab0217de
-export const PRO_2 = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
-export const PRO_3 = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
-export const PRO_4 = `I am a passionate front-end developer with strong expertise in React.js and Tailwind CSS. I have a solid foundation in HTML, CSS, and JavaScript, Bootstrap, and I specialize in building dynamic and responsive user interfaces. Additionally, I am proficient in C, C++, and Python, which complements my development skills. As a graphic designer, I also bring experience in creating visually appealing designs. My goal is to integrate these skills to deliver exceptional user experiences and innovative web applications.`;
+
+export const PRO_2 = `This project focuses on building a Typing Speed Test application using React and Tailwind CSS. It features an intuitive user interface, customizable timing options, and tracks user performance. The goal is to enhance typing speed through engaging practice sessions.`;
+
+export const PRO_3 = `Developed a Task Timer App to improve productivity, featuring work and break intervals, customizable task durations, and session tracking. Built with React, Vite, and Tailwind CSS, it allows users to manage their time effectively.`;
+
+export const PRO_4 = `Created a Personal Notes App using React and Tailwind CSS that includes task management within notes. Users can create, edit, and delete notes while tracking tasks with a visually appealing 'Death Note' aesthetic.`;
 
 export const ABOUT_TEXT = `I am a front-end developer with a deep passion for creating engaging and responsive web interfaces. My expertise lies in React.js and Tailwind CSS, and I have a strong foundation in HTML, CSS, and JavaScript. Beyond development, I am skilled in C, C++, and Python, which enhances my problem-solving capabilities. In addition to my technical skills, I am a graphic designer with a keen eye for visual aesthetics. My goal is to blend these diverse skills to craft exceptional user experiences and innovative web applications. Outside of my professional life, I enjoy playing musical instruments, including the guitar and flute. I also find joy in sketching and portrait art, which allows me to express my creativity in different ways.`;
+
 
 
 
