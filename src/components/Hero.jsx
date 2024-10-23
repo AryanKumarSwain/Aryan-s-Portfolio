759b86dfbd317572442dddbf6520c7baa99765f9
Aryan <aryannn.ks@gmail.com>
2024-10-23 20:49:55 +0530

    RESUME UP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -56,7 +56,7 @@ const Hero = () => {
           <button className="flex shrink-0 mx-20 sm:mx-44 justify-center items-center text-white text-lg rounded-full bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 font-light tracking-tighter p-4 mb-10 lg:mb-20 cursor-pointer 
 hover:shadow-[0_0_15px_rgba(255,255,255,0.8)] dark:hover:shadow-[0_0_15px_rgba(128,90,213,0.8)] transition-shadow duration-100">
   <a
-    href="https://drive.google.com/file/d/1VRooMQZLwqCJtLcAME6epPL0fqa11MNV/view?usp=sharing"
+    href="https://drive.google.com/file/d/1YKlkHUP6Vg_QqPo_2viWsE5cZRykJT4S/view?usp=sharing"
     target="_blank"
     rel="noopener noreferrer"
   >
