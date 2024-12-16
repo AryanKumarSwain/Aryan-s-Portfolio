01a87bfb39586de120072376a2fc4e9ab8fe1102
Aryan <aryannn.ks@gmail.com>
2024-12-16 21:59:24 +0530

    updated project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,36 +1,44 @@
 import React from "react";
 import pro1 from '../assets/pro1.jpg'; // Adjust the path as necessary
-import pro2 from '../assets/pro2.jpg'; // Adjust the path as necessary
+import pro2 from '../assets/pro2.png'; // Adjust the path as necessary
 import pro3 from '../assets/pro3.png'; // Adjust the path as necessary
 import pro4 from '../assets/pro4.jpg'; // Adjust the path as necessary
+import pro5 from '../assets/pro5.png'; // Adjust the path as necessary
 import Slider from "./Slider"; // Ensure the path is correct
 import { PRO_1 } from "../constants"; 
 const Projects = () => {
   const projectSlides = [
     {
       image: pro1,
-      title: "Portfolio Website",
+      title: "SWAIN Portfolio Website",
       description: PRO_1,
       link: "https://swain.netlify.app/", 
     },
     {
       image: pro4,
-      title: "Weather App",
-      description: "Project 2 description goes here.",
+      title: "SWAIN Weather App",
+      description: "Just a Weather app.😁",
       link: "https://swainweather.netlify.app/", 
     },
     {
       image: pro3,
-      title: "GPA Calculator",
-      description: "Project 3 description goes here.",
+      title: "SWAIN GPA Calculator",
+      description: "Calculate your SGPA or CGPA here.😎",
       link: "https://swaingpacalculator.netlify.app/",
     },
     {
-      image: pro4,
-      title: "Project 4",
-      description: "Project 4 description goes here.",
-      link: "https://swainweather.netlify.app/", 
+      image: pro2,
+      title: "SWAIN Type's",
+      description: "Will make it better than MONKEY TYPE.😅",
+      link: "https://swaintype.netlify.app/", 
     },
+    {
+      image: pro5,
+      title: "SWAIN Talks",
+      description: "Chat app, Will make it better than Whatsapp 😅. Ongoing...",
+      link: "https://swain.netlify.app/", 
+    },
+    
   ];
 
   return (
