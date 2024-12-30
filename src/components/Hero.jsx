771426f9b508693c62ddde9bcd093cff85edd780
Aryan <aryannn.ks@gmail.com>
2024-12-30 12:27:41 +0530

    UP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -53,8 +53,7 @@ const Hero = () => {
               {HERO_CONTENT}
             </p>
           </div>
-          <button className="flex shrink-0 mx-20 sm:mx-44 justify-center items-center text-white text-lg rounded-full bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 font-light tracking-tighter p-4 mb-10 lg:mb-20 cursor-pointer 
-hover:shadow-[0_0_15px_rgba(255,255,255,0.8)] dark:hover:shadow-[0_0_15px_rgba(128,90,213,0.8)] transition-shadow duration-100">
+          <button className="flex shrink-0 mx-20 sm:mx-44 justify-center items-center text-white text-lg rounded-full bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 font-light tracking-tighter p-4 mb-10 lg:mb-20 cursor-pointer hover:shadow-[0_0_15px_rgba(255,255,255,0.8)] dark:hover:shadow-[0_0_15px_rgba(128,90,213,0.8)] transition-shadow duration-100">
             <a
               href="https://drive.google.com/file/d/1tCrq5SKXrxm-gz6aMZJ8CPsueGIPhJAx/view?usp=sharing"
               target="_blank"
