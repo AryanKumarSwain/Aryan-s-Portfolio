5c2a066f9e1a459beb11305fd3b914b433b42a9c
Aryan <aryannn.ks@gmail.com>
2025-02-03 18:06:00 +0530

    year update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
                 </div>
             </div>
             <div className="text-center sm:flex items-center justify-between border-t border-gray-400 dark:border-gray-600 text-black dark:text-white  mx-[3%] mt-12 px-1 py-10 ">
-                <p>© 2024 SWAIN. All rights reserved.</p>
+                <p>© 2025 SWAIN. All rights reserved.</p>
                 <ul className="flex justify-center gap-4">
                     <li className=' hover:contrast-150'><a href="https://linkedin.com/in/aryannn-ks/" target="_blank"><FaLinkedin/></a></li>
                     <li className=' hover:contrast-150'><a href="https://github.com/AryanKumarSwain"target="_blank"><FaGithub/></a></li>
