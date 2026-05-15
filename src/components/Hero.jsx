2b03d6ce6b8cf9e802eff85735723fe69facc207
Aryan Kumar Swain <aryannn.ks@gmail.com>
2026-05-15 07:58:54 +0530

    resume update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -55,7 +55,7 @@ const Hero = () => {
           </div>
           <button className="flex shrink-0 mx-4 sm:mx-20 md:mx-32 lg:mx-44 justify-center items-center text-white text-lg rounded-full bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 font-light tracking-tighter p-4 mb-10 lg:mb-20 cursor-pointer hover:shadow-[0_0_15px_rgba(255,255,255,0.8)] dark:hover:shadow-[0_0_15px_rgba(128,90,213,0.8)] transition-shadow duration-100">
             <a
-              href="https://drive.google.com/file/d/1YCt-tzVE0vQQsxVLZYIoH7zE6oBbzSp_/view?usp=sharing"
+              href="https://drive.google.com/file/d/1TLQPrfkqyGtvRGLfa4c-GHpZP9Od6_lM/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
